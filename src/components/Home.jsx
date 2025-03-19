71b77d7df6cf3ced262d26bcfafaed919d3f969b
RaghavKashyap-WD <kshypraghav@gmail.com>
2025-03-19 16:11:13 +0530

    changed home changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -33,7 +33,7 @@ const Home = ({ clotheDisplayImg }) => {
       className="bg-[#E0B5DD] w-full p-10 flex flex-col justify-center items-center"
     >
       <div className="w-full p-10 flex justify-center items-center">
-        <div className="flex flex-col gap-3 max-w-[616px] max-h-[315px] justify-between mx-20">
+        <div className="flex flex-col gap-3 w-[616px] h-[315px] justify-between mx-20">
           <h1 className="font-extrabold font-[serif] text-6xl">
             Discover Your Perfect Style Today
           </h1>
@@ -43,19 +43,19 @@ const Home = ({ clotheDisplayImg }) => {
             online experience tailored just for you.
           </p>
           <div className="flex gap-[16px]">
-            <button className="p-3 bg-black hover:bg-zinc-800 transition-colors duration-300 ease-out cursor-pointer text-white max-w-[89px]">
+            <button className="p-3 bg-black hover:bg-zinc-800 transition-colors duration-300 ease-out cursor-pointer text-white w-[89px]">
               Shop
             </button>
-            <button className="bg-gray-100 hover:bg-gray-200 transition-colors duration-300 ease-out cursor-pointer max-w-[139px]">
+            <button className="bg-gray-100 hover:bg-gray-200 transition-colors duration-300 ease-out cursor-pointer w-[139px]">
               Learn More
             </button>
           </div>
         </div>
-        <div className="flex flex-col gap-2 justify-center items-center max-h-full max-w-[616px]">
+        <div className="flex flex-col gap-2 justify-center items-center h-full w-[616px]">
           <img
             src={clotheDisplayImg[index].url}
             alt=""
-            className="w-fit max-h-[80vh] object-cover rounded-xl shadow-2xl mx-28 hover:scale-105 transition-transform duration-500 ease-out cursor-pointer animate-fade-up animate-once animate-ease-in -translate-y-3"
+            className="w-fit h-[80vh] object-cover rounded-xl shadow-2xl mx-28 hover:scale-105 transition-transform duration-500 ease-out cursor-pointer animate-fade-up animate-once animate-ease-in -translate-y-3"
           />
           <div className="flex gap-3 -translate-y-1 ">
             <button
@@ -70,42 +70,42 @@ const Home = ({ clotheDisplayImg }) => {
             </button>
             <div className="flex gap-1 justify-between items-center">
               <span
-                className={`max-w-[10px] max-h-[10px] ${
+                className={`w-[10px] h-[10px] ${
                   index === 0 ? "bg-gray-800 scale-140" : "bg-gray-600"
                 } rounded-full`}
               ></span>
               <span
-                className={`max-w-[10px] max-h-[10px] ${
+                className={`w-[10px] h-[10px] ${
                   index === 1 ? "bg-gray-800 scale-140" : "bg-gray-600"
                 } rounded-full`}
               ></span>
               <span
-                className={`max-w-[10px] max-h-[10px] ${
+                className={`w-[10px] h-[10px] ${
                   index === 2 ? "bg-gray-800 scale-140" : "bg-gray-600"
                 } rounded-full`}
               ></span>
               <span
-                className={`max-w-[10px] max-h-[10px] ${
+                className={`w-[10px] h-[10px] ${
                   index === 3 ? "bg-gray-800 scale-140" : "bg-gray-600"
                 } rounded-full`}
               ></span>
               <span
-                className={`max-w-[10px] max-h-[10px] ${
+                className={`w-[10px] h-[10px] ${
                   index === 4 ? "bg-gray-800 scale-140" : "bg-gray-600"
                 } rounded-full`}
               ></span>
               <span
-                className={`max-w-[10px] max-h-[10px] ${
+                className={`w-[10px] h-[10px] ${
                   index === 5 ? "bg-gray-800 scale-140" : "bg-gray-600"
                 } rounded-full`}
               ></span>
               <span
-                className={`max-w-[10px] max-h-[10px] ${
+                className={`w-[10px] h-[10px] ${
                   index === 6 ? "bg-gray-800 scale-140" : "bg-gray-600"
                 } rounded-full`}
               ></span>
               <span
-                className={`max-w-[10px] max-h-[10px] ${
+                className={`w-[10px] h-[10px] ${
                   index === 7 ? "bg-gray-800 scale-140" : "bg-gray-600"
                 } rounded-full`}
               ></span>
